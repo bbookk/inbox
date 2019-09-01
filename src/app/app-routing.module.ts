ff3085f3c724c4f4f66c9bb7c0f3a0c73fe59f6a
bbookk <book.maytawee@gmail.com>
2019-09-01 14:45:51 +0700

    fix path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 	{
 		path: '',
-		loadChildren: 'app/component/component.module#ComponentModule'
+		loadChildren: './component/component.module#ComponentModule'
 	}
 ];
 
